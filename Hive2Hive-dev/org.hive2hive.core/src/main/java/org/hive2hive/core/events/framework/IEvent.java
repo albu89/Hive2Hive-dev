84b2a50d41202987563ee537df7e398f6798384b
Alexander Büsser <alexanderbusser@tsf-476-wpa-7-087.epfl.ch>
2014-11-10 15:48:50 +0100

    Remove duplicate directory

--- Hive2Hive-dev/org.hive2hive.core/src/main/java/org/hive2hive/core/events/framework/IEvent.java
+++
@@ -1,5 +0,0 @@
-package org.hive2hive.core.events.framework;
-
-public interface IEvent {
-	// so far, just a marker interface
-}
--- Hive2Hive-dev/org.hive2hive.core/src/main/java/org/hive2hive/core/network/data/download/IDownloadListener.java
+++
@@ -1,8 +0,0 @@
-package org.hive2hive.core.network.data.download;
-
-public interface IDownloadListener {
-
-	void downloadFinished(BaseDownloadTask task);
-
-	void downloadFailed(BaseDownloadTask task, String reason);
-}
